3b8cc647d6befb0d436d78eb563814b3922d5ec5
Andrew Lee <leeandrew781@gmail.com>
2015-12-04 19:13:12 -0800

    // Finished mapping the Aplhanumeric digits to our key of binary digits via a massive switch statement // Next up is combining the strings into one and dividing that into numbers to use as a key

--- HashTable.java
+++ HashTable.java
@@ -1,7 +1,24 @@
-// HashTable function
-
-import java.util.*;
+//#################################
+//# Name: Andrew Lee & Leo Gomez  #
+//# CruzID: ale915@ & legomez@    #
+//# Class: CMPS101-Spr15          #
+//# Date: Dec 3, 2015             #
+//# filename: HashTable.java      #
+//# Details: Creates a customer id#
+//# and adds it into a hashtable. #
+//#################################
+//
+import java.io.File;
+import static java.lang.System.*;
+import java.io.*;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
 import java.util.Scanner;
+import java.util.*;
+import java.util.Random;
+
 
 public class HashTable {
 
@@ -37,18 +54,178 @@ public class HashTable {
       String str;
       double bal;
       
+      // Generating random number
+//////////////////////////////////////////////////////
+
+	for (int i = 0; i<userin.length(); i++){
+	 char c = userin.charAt(i);
+	 System.out.println ("Iterated char= "+c);
+	 
+	 String charString;
+	 switch (c) {
+            case '0':  charString = "000001";
+                     break;
+            case '1':  charString = "000010";
+                     break;
+            case '2':  charString = "000011";
+                     break;
+            case '3':  charString = "000100";
+                     break;
+            case '4':  charString = "000101";
+                     break;
+            case '5':  charString = "000110";
+                     break;
+            case '6':  charString = "000111";
+                     break;
+            case '7':  charString = "001000";
+                     break;
+            case '8':  charString = "001001";
+                     break;
+            case '9':  charString = "001010";
+                     break;
+            case 'a': charString = "001011";
+                     break;
+            case 'b': charString = "001100";
+                     break;
+	    case 'c': charString = "001101";
+                     break;
+	    case 'd': charString = "001110";
+                     break;
+            case 'e': charString = "001111";
+                     break;
+            case 'f': charString = "010000";
+                     break;
+	    case 'g': charString = "010001";
+                     break;
+            case 'h': charString = "010010";
+                     break;
+            case 'i': charString = "010011";
+                     break;
+            case 'j': charString = "010100";
+                     break;
+            case 'k': charString = "010101";
+                     break;
+            case 'l': charString = "010110";
+                     break;
+            case 'm': charString = "010111";
+                     break;
+            case 'n': charString = "011000";
+                     break;
+            case 'o': charString = "011001";
+                     break;
+            case 'p': charString = "011010";
+                     break;
+            case 'q': charString = "011011";
+                     break;
+            case 'r': charString = "011100";
+                     break;
+	    case 's': charString = "011101";
+                     break;
+            case 't': charString = "011110";
+                     break;
+            case 'u': charString = "011111";
+                     break;
+	    case 'v': charString = "100000";
+                     break;
+            case 'w': charString = "100001";
+                     break;
+            case 'x': charString = "100010";
+                     break;
+	    case 'y': charString = "100011";
+                     break;
+            case 'z': charString = "100100";
+                     break;
+            case 'A': charString = "100101";
+                     break;
+	    case 'B': charString = "100110";
+                     break;
+	    case 'C': charString = "100111";
+                     break;
+	    case 'D': charString = "101000";
+                     break;
+            case 'E': charString = "101001";
+                     break;
+            case 'F': charString = "101010";
+                     break;
+	    case 'G': charString = "101011";
+                     break;
+            case 'H': charString = "101100";
+                     break;
+            case 'I': charString = "101101";
+                     break;
+            case 'J': charString = "101110";
+                     break;
+            case 'K': charString = "101111";
+                     break;
+            case 'L': charString = "110000";
+                     break;
+	    case 'M': charString = "110001";
+                     break;
+            case 'N': charString = "110010";
+                     break;
+            case 'O': charString = "110011";
+                     break;
+            case 'P': charString = "110100";
+                     break;
+            case 'Q': charString = "110101";
+                     break;
+            case 'R': charString = "110110";
+                     break;
+            case 'S': charString = "110111";
+                     break;
+            case 'T': charString = "111000";
+                     break;
+            case 'U': charString = "111001";
+                     break;
+            case 'V': charString = "111010";
+                     break;
+            case 'W': charString = "111011";
+                     break;
+            case 'X': charString = "111100";
+                     break;
+	    case 'Y': charString = "111101";
+                     break;
+            case 'Z': charString = "111110";
+                     break;
+            default: charString  = "Incorrect Digit";
+                     break;
+         }
+	  System.out.println("Bit translated to: " +charString);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////
+      // Entering new user into hashtable
+      balance.put(userin, new Double(300.00));
+      
       balance.put("Zara", new Double(3434.34));
       balance.put("Mahnaz", new Double(123.22));
-      balance.put("Ayan", new Double(1378.00));
-      balance.put("Daisy", new Double(99.22));
-      balance.put("Qadir", new Double(-19.08));
 
       // Show all balances in hash table.
       names = balance.keys();
       while(names.hasMoreElements()) {
+	 System.out.println("");
          str = (String) names.nextElement();
-         System.out.println(str + ": " +
-         balance.get(str));
+//         System.out.println(str + ": " +
+//         balance.get(str));
       }
       System.out.println();
       // Deposit 1,000 into Zara's account
@@ -67,3 +244,5 @@ public class HashTable {
         return false;   
    }
 }
+
+
